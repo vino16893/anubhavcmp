4e15d3771070622919acfd9974e1b42bf941da99
Administrator <Administrator@Quality-PC>
2022-09-12 13:29:41 +0530

    [vino]code updated

---
+++ src/anubhav/sample1.java
@@ -0,0 +1,11 @@
+package anubhav;
+
+public class sample1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("module 2 is under verification");
+	}
+
+
+}
---
+++ src/anubhav/sample2.java
@@ -0,0 +1,10 @@
+package anubhav;
+
+public class sample2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("module 1 is under verification");
+	}
+
+}
