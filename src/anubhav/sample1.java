e48e4994cb29b0ae69502988168fc83a85beea99
Administrator <Administrator@Quality-PC>
2022-09-12 13:33:16 +0530

    [vino]completed

--- src/anubhav/sample1.java
+++ src/anubhav/sample1.java
@@ -4,7 +4,7 @@ public class sample1
 {
 	public static void main(String[] args)
 	{
-		System.out.println("module 2 is under verification");
+		System.out.println("module 2 is completed");
 	}
 
 
--- src/anubhav/sample2.java
+++ src/anubhav/sample2.java
@@ -4,7 +4,7 @@ public class sample2
 {
 	public static void main(String[] args)
 	{
-		System.out.println("module 1 is under verification");
+		System.out.println("module 1 is complete");
 	}
 
 }
